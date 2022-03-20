9bc60a245ee0525f7c9787415bd90b6d2a31bc97
EFREN <efrennm86@gmail.com>
2022-03-19 19:40:23 -0600

    ESPECIALES/MAYUS/NUMS

--- passwords/generator/views.py
+++ passwords/generator/views.py
@@ -1,3 +1,4 @@
+from webbrowser import get
 from django.shortcuts import render
 from django.http import HttpResponse
 import random
@@ -11,10 +12,21 @@ def home(request):
     return render(request, 'home.html')
 
 def password(request):
-    caracteres= list('abcdeefghijklmnñopqrstuvxyz')
+    caracteres= list('abcdefghijklmnñopqrstuvxyz')
+    
+    if 'especiales' in request.POST:
+        caracteres.extend(list('!"#$%&/()'))
+
+    if 'uppercase' in request.POST:
+        caracteres.extend(list('ABCDEFGHIJKLMNÑOPQRSTUVXYZ'))
+
+    if 'numeros' in request.POST:
+        caracteres.extend(list('0123456789'))
+        
     longitud= int(request.POST['length'])
     password_generado=''
+    
     for x in range(longitud):
         password_generado+=random.choice(caracteres)
-
+    
     return render(request, 'password.html', {'password':password_generado})
--- passwords/passwords/urls.py
+++ passwords/passwords/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', views.home),
     path('about/', views.about),
-    path('password/', views.password)
+    path('password/', views.password, name='password')
 ]
