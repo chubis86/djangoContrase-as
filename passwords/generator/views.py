4683dfa10ef25fdb7b0a29ac654a788479734a4b
EFREN <efrennm86@gmail.com>
2022-03-19 18:56:27 -0600

    Vista password modificada

--- passwords/generator/views.py
+++ passwords/generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
@@ -10,4 +11,10 @@ def home(request):
     return render(request, 'home.html')
 
 def password(request):
-    return render(request, 'password.html')
+    caracteres= list('abcdeefghijklmnñopqrstuvxyz')
+    longitud= int(request.POST['length'])
+    password_generado=''
+    for x in range(longitud):
+        password_generado+=random.choice(caracteres)
+
+    return render(request, 'password.html', {'password':password_generado})
